2aa97735bbc49c075df9f5aaacbb1303a2009a8e
José Alberto <albertomajalca159@gmail.com>
2025-02-27 15:41:49 -0600

    Deploy a pages

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: 'https://betocas11.github.io/eccomerce-react-tailwind-vite/' 
 })
