5b1c149f499d126f1125015e3f06769bc806d39c
José Alberto <albertomajalca159@gmail.com>
2025-02-27 16:00:34 -0600

    más cambios

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: 'https://betocas11.github.io/eccomerce-react-tailwind-vite/' 
+  base: '/eccomerce-react-tailwind-vite/' 
 })
